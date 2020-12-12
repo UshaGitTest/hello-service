9941f2121b10640ea76a34c060d28dcd2c5b057b
usha <usha.ammaresh@gmail.com>
2020-12-12 12:13:18 -0600

    Updated hello message

--- src/main/java/com/cscie90/HelloWorldController.java
+++ src/main/java/com/cscie90/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
     @GetMapping({"/", "hello"})
     public String helloWorld() {
-        return "hello-world-again";
+        return "hello-world";
     }
 }
  
